c7e641ec61740b5b92aa5ea519a987698e402ab0
Adolfo_Tun <60202428+AdolfoMX@users.noreply.github.com>
2021-04-23 19:10:56 -0500

    corrección código

--- programas/ensamblador/funciones_booleanas_1.asm
+++ programas/ensamblador/funciones_booleanas_1.asm
@@ -3,10 +3,6 @@
 
 ;------------- Seccion de datos -------------
 section .data
-; declaracion de caracteres escape
-ln db 10, 13
-longln equ 2
-
 ; declaracion de mensajes si es par o impar el numero
 msg1 db "numero par", 0xa, 0xd
 longmsg1 equ $-msg1
@@ -21,20 +17,33 @@ section .text
     global _start
 
 start:
-mov al, 5  ; mueve y almacena el valor 5 en "al"
-mov bl, 1  ; mueve y almacena el valor 1 en "bl"
+; si desea cambiar el valor de comparacion solo reemplaze el numero 5
+; el valor de 1 del registro bl sirve para la comparacion de si es impar
+mov al, 5            ; mueve y almacena el valor 5 en "al"
+mov bl, 1            ; mueve y almacena el valor 1 en "bl"
 
-and al, bl ; comparacion logica "and" que se ejecuta mediante codigo binario
+and al, bl           ; comparacion logica "and" que se ejecuta mediante codigo binario
+jz numeropar         ; bandera jz que manda un mensaje si el numero es par
 
-
-
-; ---- impresion de escape ----
-mov eax, 4
-mov ebx, 1
-mov ecx, ln
-mov edx, longln
+; si el numero no es par manda un mensaje con el siguiente codigo
+mov eax, 4           ; indica a la maquina que se va a escribir
+mov ebx, 1           ; indica a la maquina que se va a escribir en pantalla
+mov ecx, msg2        ; indica la direccion de memoria donde esta almacenado la variable msg2
+mov edx, longmsg2    ; numero de bites a imprimir
 int 0x80
 
+; para evitar que mande los mensajes al mismo tiempo se manda un salto de linea
+jmp salir
+
+numeropar:
+; manda un mensaje del numero que es par
+mov eax, 4           ; indica a la maquina que se va a escribir
+mov ebx, 1           ; indica a la maquina que se va a escribir en pantalla
+mov ecx, msg1        ; indica la direccion de memoria donde esta almacenado la variable msg1
+mov edx, longmsg1    ; numero de bites a imprimir
+int 0x80
+
+salir:
 ; indica a la maquina si el programa se ejecuto de forma exitosa
 mov eax, 1
 mov ebx, 0
