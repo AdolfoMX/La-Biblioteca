b0dd4250435c0cad18d0f4510bf88f7bb8cd8bea
Adolfo_Tun <60202428+AdolfoMX@users.noreply.github.com>
2021-05-06 21:32:10 -0500

    sentencia if-else en ensamblador

---
+++ programas/ensamblador/estructuras_de_decisión_2.asm
@@ -0,0 +1,39 @@
+; Estructuras de decisión parte 2
+; Simulando la sentencia if-else
+
+;------------- Seccion de datos -------------
+section .bss
+resultado resb 1 ; reserva de memoria de un byte
+
+;------------- Seccion de texto -------------
+section .text
+    global _start
+
+_start:
+mov eax, 5             ; almacenando el valor 5
+mov ebx, 9             ; almacenando el valor 9
+
+cmp eax, 5             ; comparación del registro eax con 5
+jz cuerpoif            ; si la comparación es igual manda un 1 desde cuerpoif
+
+cuerpoelse:            ; condicional si no se cumple la función cuerpoif
+mov ebx, 0             ; almacena un valor 0 cuando el valor no es igual
+jmp salir              ; si la comparacion no es igual se termina el programa imprimiendo el numero cero
+
+cuerpoif:
+mov ebx, 1             ; almacenando el valor 1
+
+salir:
+add ebx, 48            ; suma de código ascii para tener el valor de la comparación
+mov [resultado], ebx   ; el resultado de la suma se almacena en la variable "resultado"
+
+mov eax, 4             ; indica a la maquina que se va a escribir
+mov ebx, 1             ; indica a la maquina que se va a escribir en pantalla
+mov ecx, resultado     ; indica la direccion de memoria donde esta almacenado la variable resultado
+mov edx, 1             ; numero de bites a imprimir
+int 0x80
+
+; indica a la maquina si el programa se ejecuto de forma exitosa
+mov eax, 1
+mov ebx, 0
+int 0x80
